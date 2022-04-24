28c661de9ebf5e2527992432b60848c8ab222456
Eunice Dhivya <divi.seed@gmail.com>
2022-04-25 01:54:40 +0530

    add scrolling for photogalleyr

--- scroll-app.js
+++ scroll-app.js
@@ -671,3 +671,155 @@ var pinOnly4 = ScrollTrigger.create({
   end: "+=600%",
   pinSpacing: true,
 });
+
+var tl5 = gsap.timeline();
+
+// 1
+tl5
+  .from(".swiper-slide1", { opacity: 1 }, "one")
+  .to(".sec6Page1", { backgroundColor: "#000", scale: 2 }, "one")
+  .to(".sec6Page1", { backgroundColor: "#c4c4c4", scale: 1 }, "one-end")
+  .to(".swiper-slide1", { opacity: 0 }, "one-end");
+
+// 2
+tl5
+  .from(".swiper-slide2", { opacity: 0 }, "two")
+  .to(".sec6Page2", { backgroundColor: "#000", scale: 2 }, "two")
+  .to(".sec6Page2", { backgroundColor: "#c4c4c4", scale: 1 }, "two-end")
+  .to(".swiper-slide2", { opacity: 0 }, "two-end");
+
+// 3
+tl5
+  .from(".swiper-slide3", { opacity: 0 }, "three")
+  .to(".sec6Page3", { backgroundColor: "#000", scale: 2 }, "three")
+  .to(".sec6Page3", { backgroundColor: "#c4c4c4", scale: 1 }, "three-end")
+  .to(".swiper-slide3", { opacity: 0 }, "three-end");
+
+// 4
+tl5
+  .from(".swiper-slide4", { opacity: 0 }, "four")
+  .to(".sec6Page4", { backgroundColor: "#000", scale: 2 }, "four")
+  .to(".sec6Page4", { backgroundColor: "#c4c4c4", scale: 1 }, "four-end")
+  .to(".swiper-slide4", { opacity: 0 }, "four-end");
+
+// 5
+tl5
+  .from(".swiper-slide5", { opacity: 0 }, "five")
+  .to(".sec6Page5", { backgroundColor: "#000", scale: 2 }, "five")
+  .to(".sec6Page5", { backgroundColor: "#c4c4c4", scale: 1 }, "five-end")
+  .to(".swiper-slide5", { opacity: 0 }, "five-end");
+
+// 6
+tl5
+  .from(".swiper-slide6", { opacity: 0 }, "six")
+  .to(".sec6Page6", { backgroundColor: "#000", scale: 2 }, "six")
+  .to(".sec6Page6", { backgroundColor: "#c4c4c4", scale: 1 }, "six-end")
+  .to(".swiper-slide6", { opacity: 0 }, "six-end");
+
+// 7
+tl5
+  .from(".swiper-slide7", { opacity: 0 }, "seven")
+  .to(".sec6Page7", { backgroundColor: "#000", scale: 2 }, "seven")
+  .to(".sec6Page7", { backgroundColor: "#c4c4c4", scale: 1 }, "seven-end")
+  .to(".swiper-slide7", { opacity: 0 }, "seven-end");
+
+// 8
+tl5
+  .from(".swiper-slide8", { opacity: 0 }, "eight")
+  .to(".sec6Page8", { backgroundColor: "#000", scale: 2 }, "eight")
+  .to(".sec6Page8", { backgroundColor: "#c4c4c4", scale: 1 }, "eight-end")
+  .to(".swiper-slide8", { opacity: 0 }, "eight-end");
+
+// 9
+tl5
+  .from(".swiper-slide9", { opacity: 0 }, "nine")
+  .to(".sec6Page9", { backgroundColor: "#000", scale: 2 }, "nine")
+  .to(".sec6Page9", { backgroundColor: "#c4c4c4", scale: 1 }, "nine-end")
+  .to(".swiper-slide9", { opacity: 0 }, "nine-end");
+
+// 10
+tl5
+  .from(".swiper-slide10", { opacity: 0 }, "ten")
+  .to(".sec6Page10", { backgroundColor: "#000", scale: 2 }, "ten")
+  .to(".sec6Page10", { backgroundColor: "#c4c4c4", scale: 1 }, "ten-end")
+  .to(".swiper-slide10", { opacity: 0 }, "ten-end");
+
+// 11
+tl5
+  .from(".swiper-slide11", { opacity: 0 }, "eleven")
+  .to(".sec6Page11", { backgroundColor: "#000", scale: 2 }, "eleven")
+  .to(".sec6Page11", { backgroundColor: "#c4c4c4", scale: 1 }, "eleven-end")
+  .to(".swiper-slide11", { opacity: 0 }, "eleven-end");
+
+// 12
+tl5
+  .from(".swiper-slide12", { opacity: 0 }, "twelve")
+  .to(".sec6Page12", { backgroundColor: "#000", scale: 2 }, "twelve")
+  .to(".sec6Page12", { backgroundColor: "#c4c4c4", scale: 1 }, "twelve-end")
+  .to(".swiper-slide12", { opacity: 0 }, "twelve-end");
+
+// 13
+tl5
+  .from(".swiper-slide13", { opacity: 0 }, "thirteen")
+  .to(".sec6Page13", { backgroundColor: "#000", scale: 2 }, "thirteen")
+  .to(".sec6Page13", { backgroundColor: "#c4c4c4", scale: 1 }, "thirteen-end")
+  .to(".swiper-slide13", { opacity: 0 }, "thirteen-end");
+
+// 14
+tl5
+  .from(".swiper-slide14", { opacity: 0 }, "fourteen")
+  .to(".sec6Page14", { backgroundColor: "#000", scale: 2 }, "fourteen")
+  .to(".sec6Page14", { backgroundColor: "#c4c4c4", scale: 1 }, "fourteen-end")
+  .to(".swiper-slide14", { opacity: 0 }, "fourteen-end");
+
+// 15
+tl5
+  .from(".swiper-slide15", { opacity: 0 }, "fifteen")
+  .to(".sec6Page15", { backgroundColor: "#000", scale: 2 }, "fifteen")
+  .to(".sec6Page15", { backgroundColor: "#c4c4c4", scale: 1 }, "fifteen-end")
+  .to(".swiper-slide15", { opacity: 0 }, "fifteen-end");
+
+// 16
+tl5
+  .from(".swiper-slide16", { opacity: 0 }, "sixteen")
+  .to(".sec6Page16", { backgroundColor: "#000", scale: 2 }, "sixteen")
+  .to(".sec6Page16", { backgroundColor: "#c4c4c4", scale: 1 }, "sixteen-end")
+  .to(".swiper-slide16", { opacity: 0 }, "sixteen-end");
+
+// 17
+tl5
+  .from(".swiper-slide17", { opacity: 0 }, "seventeen")
+  .to(".sec6Page17", { backgroundColor: "#000", scale: 2 }, "seventeen")
+  .to(".sec6Page17", { backgroundColor: "#c4c4c4", scale: 1 }, "seventeen-end")
+  .to(".swiper-slide17", { opacity: 0 }, "seventeen-end");
+
+// 18
+tl5
+  .from(".swiper-slide18", { opacity: 0 }, "eighteen")
+  .to(".sec6Page18", { backgroundColor: "#000", scale: 2 }, "eighteen")
+  .to(".sec6Page18", { backgroundColor: "#c4c4c4", scale: 1 }, "eighteen-end")
+  .to(".swiper-slide18", { opacity: 0 }, "eighteen-end");
+
+// 19
+tl5
+  .from(".swiper-slide19", { opacity: 0 }, "nineteen")
+  .to(".sec6Page19", { backgroundColor: "#000", scale: 2 }, "nineteen")
+  .to(".sec6Page19", { backgroundColor: "#c4c4c4", scale: 1 }, "nineteen-end")
+  .to(".swiper-slide19", { opacity: 0 }, "nineteen-end");
+
+// 20
+tl5
+  .from(".swiper-slide20", { opacity: 0 }, "twenty")
+  .to(".sec6Page20", { backgroundColor: "#000", scale: 2 }, "twenty")
+  .to(".sec6Page20", { backgroundColor: "#c4c4c4", scale: 1 }, "twenty-end")
+  .to(".swiper-slide20", { opacity: 0 }, "twenty-end");
+
+var pinOnly5 = ScrollTrigger.create({
+  animation: tl5,
+  trigger: ".interactive_photogallery",
+  pin: true,
+  scrub: true,
+  start: "top top",
+  end: "+=600%",
+  pinSpacing: true,
+});
