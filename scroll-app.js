62872e9a07266a88d7262ba40429db2baf97a4bf
Eunice Dhivya <divi.seed@gmail.com>
2022-04-25 21:14:56 +0530

    replace toe section

--- scroll-app.js
+++ scroll-app.js
@@ -19,104 +19,104 @@ sections.forEach((section) => {
 // Timeline of Events ========================= //
 // ============================================ //
 
-var tl = gsap.timeline();
+// var tl = gsap.timeline();
 
-var activeState = { color: "red", fontSize: "17px", fontWeight: "bold" };
-var inActiveState = { color: "grey", fontSize: "12px" };
+// var activeState = { color: "red", fontSize: "17px", fontWeight: "bold" };
+// var inActiveState = { color: "grey", fontSize: "12px" };
 
-// 1
-tl.from(".sec1-slide1", { opacity: 1 }, "one")
-  .to(".sec1-slide1", { opacity: 1 })
-  .to(".sec1-slide1", { opacity: 0 }, "one-end")
-  .to("#dec2021", activeState, "one")
-  .from(".sec1-years", { opacity: 1 }, "one")
-  .to("#dec2021", inActiveState, "one-end");
+// // 1
+// tl.from(".sec1-slide1", { opacity: 1 }, "one")
+//   .to(".sec1-slide1", { opacity: 1 })
+//   .to(".sec1-slide1", { opacity: 0 }, "one-end")
+//   .to("#dec2021", activeState, "one")
+//   .from(".sec1-years", { opacity: 1 }, "one")
+//   .to("#dec2021", inActiveState, "one-end");
 
-// 2
-tl.from(".sec1-slide2", { opacity: 0 }, "two")
-  .to(".sec1-slide2", { opacity: 1 })
-  .to(".sec1-slide2", { opacity: 0 }, "two-end")
-  .to("#feb2022", activeState, "two")
-  .to(".sec1-years", { x: "-124px" }, "two")
-  .to("#feb2022", inActiveState, "two-end");
+// // 2
+// tl.from(".sec1-slide2", { opacity: 0 }, "two")
+//   .to(".sec1-slide2", { opacity: 1 })
+//   .to(".sec1-slide2", { opacity: 0 }, "two-end")
+//   .to("#feb2022", activeState, "two")
+//   .to(".sec1-years", { x: "-124px" }, "two")
+//   .to("#feb2022", inActiveState, "two-end");
 
-// 3
-tl.from(".sec1-slide3", { opacity: 0 }, "three")
-  .to(".sec1-slide3", { opacity: 1 })
-  .to(".sec1-slide3", { opacity: 0 }, "three-end")
-  .to("#feb22-2022", activeState, "three")
-  .to(".sec1-years", { x: "-224px" }, "three")
-  .to("#feb22-2022", inActiveState, "three-end");
+// // 3
+// tl.from(".sec1-slide3", { opacity: 0 }, "three")
+//   .to(".sec1-slide3", { opacity: 1 })
+//   .to(".sec1-slide3", { opacity: 0 }, "three-end")
+//   .to("#feb22-2022", activeState, "three")
+//   .to(".sec1-years", { x: "-224px" }, "three")
+//   .to("#feb22-2022", inActiveState, "three-end");
 
-// 4
-tl.from(".sec1-slide4", { opacity: 0 }, "four")
-  .to(".sec1-slide4", { opacity: 1 })
-  .to(".sec1-slide4", { opacity: 0 }, "four-end")
-  .to("#feb24-2022", activeState, "four")
-  .to(".sec1-years", { x: "-324px" }, "four")
-  .to("#feb24-2022", inActiveState, "four-end");
+// // 4
+// tl.from(".sec1-slide4", { opacity: 0 }, "four")
+//   .to(".sec1-slide4", { opacity: 1 })
+//   .to(".sec1-slide4", { opacity: 0 }, "four-end")
+//   .to("#feb24-2022", activeState, "four")
+//   .to(".sec1-years", { x: "-324px" }, "four")
+//   .to("#feb24-2022", inActiveState, "four-end");
 
-// 5
-tl.from(".sec1-slide5", { opacity: 0 }, "five")
-  .to(".sec1-slide5", { opacity: 1 })
-  .to(".sec1-slide5", { opacity: 0 }, "five-end")
-  .to("#feb26-2022", activeState, "five")
-  .to(".sec1-years", { x: "-424px" }, "five")
-  .to("#feb26-2022", inActiveState, "five-end");
+// // 5
+// tl.from(".sec1-slide5", { opacity: 0 }, "five")
+//   .to(".sec1-slide5", { opacity: 1 })
+//   .to(".sec1-slide5", { opacity: 0 }, "five-end")
+//   .to("#feb26-2022", activeState, "five")
+//   .to(".sec1-years", { x: "-424px" }, "five")
+//   .to("#feb26-2022", inActiveState, "five-end");
 
-// 6
-tl.from(".sec1-slide6", { opacity: 0 }, "six")
-  .to(".sec1-slide6", { opacity: 1 })
-  .to(".sec1-slide6", { opacity: 0 }, "six-end")
-  .to("#mar02-2022", activeState, "six")
-  .to(".sec1-years", { x: "-524px" }, "six")
-  .to("#mar02-2022", inActiveState, "six-end");
+// // 6
+// tl.from(".sec1-slide6", { opacity: 0 }, "six")
+//   .to(".sec1-slide6", { opacity: 1 })
+//   .to(".sec1-slide6", { opacity: 0 }, "six-end")
+//   .to("#mar02-2022", activeState, "six")
+//   .to(".sec1-years", { x: "-524px" }, "six")
+//   .to("#mar02-2022", inActiveState, "six-end");
 
-// 7
-tl.from(".sec1-slide7", { opacity: 0 }, "seven")
-  .to(".sec1-slide7", { opacity: 1 })
-  .to(".sec1-slide7", { opacity: 0 }, "seven-end")
-  .to("#mar26-2022", activeState, "seven")
-  .to(".sec1-years", { x: "-624px" }, "seven")
-  .to("#mar26-2022", inActiveState, "seven-end");
+// // 7
+// tl.from(".sec1-slide7", { opacity: 0 }, "seven")
+//   .to(".sec1-slide7", { opacity: 1 })
+//   .to(".sec1-slide7", { opacity: 0 }, "seven-end")
+//   .to("#mar26-2022", activeState, "seven")
+//   .to(".sec1-years", { x: "-624px" }, "seven")
+//   .to("#mar26-2022", inActiveState, "seven-end");
 
-// 8
-tl.from(".sec1-slide8", { opacity: 0 }, "eight")
-  .to(".sec1-slide8", { opacity: 1 })
-  .to(".sec1-slide8", { opacity: 0 }, "eight-end")
-  .to("#mar31-2022", activeState, "eight")
-  .to(".sec1-years", { x: "-724px" }, "eight")
-  .to("#mar31-2022", inActiveState, "eight-end");
+// // 8
+// tl.from(".sec1-slide8", { opacity: 0 }, "eight")
+//   .to(".sec1-slide8", { opacity: 1 })
+//   .to(".sec1-slide8", { opacity: 0 }, "eight-end")
+//   .to("#mar31-2022", activeState, "eight")
+//   .to(".sec1-years", { x: "-724px" }, "eight")
+//   .to("#mar31-2022", inActiveState, "eight-end");
 
-// 9
-tl.from(".sec1-slide9", { opacity: 0 }, "nine")
-  .to(".sec1-slide9", { opacity: 1 })
-  .to(".sec1-slide9", { opacity: 0 }, "nine-end")
-  .to("#apr02-2022", activeState, "nine")
-  .to(".sec1-years", { x: "-824px" }, "nine")
-  .to("#apr02-2022", inActiveState, "nine-end");
+// // 9
+// tl.from(".sec1-slide9", { opacity: 0 }, "nine")
+//   .to(".sec1-slide9", { opacity: 1 })
+//   .to(".sec1-slide9", { opacity: 0 }, "nine-end")
+//   .to("#apr02-2022", activeState, "nine")
+//   .to(".sec1-years", { x: "-824px" }, "nine")
+//   .to("#apr02-2022", inActiveState, "nine-end");
 
-// 10
-tl.from(".sec1-slide10", { opacity: 0 }, "ten")
-  .to(".sec1-slide10", { opacity: 1 })
-  .to(".sec1-slide10", { opacity: 0 }, "ten-end")
-  .to("#apr03-2022", activeState, "ten")
-  .to(".sec1-years", { x: "-924px" }, "ten")
-  .to("#apr03-2022", inActiveState, "ten-end");
+// // 10
+// tl.from(".sec1-slide10", { opacity: 0 }, "ten")
+//   .to(".sec1-slide10", { opacity: 1 })
+//   .to(".sec1-slide10", { opacity: 0 }, "ten-end")
+//   .to("#apr03-2022", activeState, "ten")
+//   .to(".sec1-years", { x: "-924px" }, "ten")
+//   .to("#apr03-2022", inActiveState, "ten-end");
 
-// 11
-tl.from(".sec1-slide11", { opacity: 0 }, "eleven")
-  .to(".sec1-slide11", { opacity: 1 })
-  .to(".sec1-slide11", { opacity: 0 }, "eleven-end")
-  .to("#apr04-2022", activeState, "eleven")
-  .to(".sec1-years", { x: "-1124px" }, "eleven")
-  .to("#apr04-2022", inActiveState, "eleven-end");
+// // 11
+// tl.from(".sec1-slide11", { opacity: 0 }, "eleven")
+//   .to(".sec1-slide11", { opacity: 1 })
+//   .to(".sec1-slide11", { opacity: 0 }, "eleven-end")
+//   .to("#apr04-2022", activeState, "eleven")
+//   .to(".sec1-years", { x: "-1124px" }, "eleven")
+//   .to("#apr04-2022", inActiveState, "eleven-end");
 
-// 12
-tl.from(".sec1-slide12", { opacity: 0 }, "twelve")
-  .to(".sec1-slide12", { opacity: 1 })
-  .to("#apr10-2022", activeState, "twelve")
-  .to(".sec1-years", { x: "-1224px" }, "twelve");
+// // 12
+// tl.from(".sec1-slide12", { opacity: 0 }, "twelve")
+//   .to(".sec1-slide12", { opacity: 1 })
+//   .to("#apr10-2022", activeState, "twelve")
+//   .to(".sec1-years", { x: "-1224px" }, "twelve");
 
 // tl.from(".sec1-slide12", { opacity: 0 }, "twelve")
 //   .to(".sec1-slide12", { opacity: 1 })
@@ -125,15 +125,15 @@ tl.from(".sec1-slide12", { opacity: 0 }, "twelve")
 //   .to(".sec1-years", { x: "-1224px" }, "twelve")
 //   .to("#apr10-2022", inActiveState, "twelve-end");
 
-var pinOnly = ScrollTrigger.create({
-  animation: tl,
-  trigger: ".interective__timeline",
-  pin: true,
-  scrub: true,
-  start: "top top",
-  end: "+=600%",
-  pinSpacing: true,
-});
+// var pinOnly = ScrollTrigger.create({
+//   animation: tl,
+//   trigger: ".interective__timeline",
+//   pin: true,
+//   scrub: true,
+//   start: "top top",
+//   end: "+=600%",
+//   pinSpacing: true,
+// });
 
 // Timeline of Invasion ========================== //
 // =============================================== //
